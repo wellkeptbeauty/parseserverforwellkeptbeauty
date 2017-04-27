fed30a5448737a14b19503354d43bdaedd3996fa
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-04-27 11:59:55 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,34 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+Parse.Cloud.define('All', function(request, status)  
+{
+  var query=new Parse.Query("MyCollection");
+   query.find().then(function (res) 
+{
+for (var i=0; i< res.length;i++){
+var expirydate=res[i].get('PExpirationDate');
+ 
+var inputDate = new Date(expirydate);
+var todaysDate = new Date();
+
+if((inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)))
+{
+
+res.success("object id is"+res[i].get('PurchasedUserID'));
+}
+else
+{
+
+alert(" not equal")
+}
+}
+ status.success("final result " + results);
+        
+      
+   
+    }, function queryFailed(reason) {
+      status.error("query unsuccessful, length of result " + result.length + ", error:" + error.code + " " + error.message);
+         
+    });
+});
