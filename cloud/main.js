ad73e8815d554a4564baee4551b229b83f6a192e
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-04-27 17:09:02 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -6,14 +6,14 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define("averageStars", function(request, response) {
   var query = new Parse.Query("MyCollection");
- // query.equalTo("movie", request.params.movie);
+  
   query.find({
     success: function(results) {
-//       var sum = 0;
-//       for (var i = 0; i < results.length; ++i) {
-//         sum += results[i].get("stars");
-//       }
-      response.success(results);
+      
+      for (var i = 0; i < results.length; ++i) {
+        string = results[i].get("PExpirationDate");
+      }
+      response.success(string);
     },
     error: function() {
       response.error("movie lookup failed");
