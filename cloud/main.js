0e8608142abd6bc70ddfa28e072fc978e41d455c
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-04-27 17:23:34 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -7,21 +7,21 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define("averageStars", function(request, response) {
   
   var GameScore = Parse.Object.extend("MyCollection");
-
-  var query = new Parse.Query(GameScore);
-  
-  query.find({
-    success: function(results) {
-      
-//       for (var i = 0; i < results.length; ++i) {
-//         string = results[i].get("PExpirationDate");
-//       }
-      response.success(results);
-    },
-    error: function() {
-      response.error("movie lookup failed");
-    }
-  });
+var query = new Parse.Query(GameScore);
+//query.equalTo("playerName", "Dan Stemkoski");
+query.find({
+  success: function(results) {
+   // alert("Successfully retrieved " + results.length + " scores.");
+    // Do something with the returned Parse.Object values
+   for (var i = 0; i < results.length; i++) { 
+     var object = results[i];
+     console.log(object.id + ' - ' + object.get('PExpirationDate'));
+   }
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
 });
 
 Parse.Cloud.define('All', function(request, status)  
