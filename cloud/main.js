e573516a125e428a2d9941910763de6f233ac181
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-04-28 11:15:55 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,22 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
+
+
+Parse.Cloud.define("averageStars", function(request, response) {
+  var query = new Parse.Query("MyCollection");
+ 
+  query.find({
+    success: function(results) {
+      
+      response.success(results);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
+});
+
 Parse.Cloud.define('All', function(request, status)  
 {
    // res.success('ALL');
