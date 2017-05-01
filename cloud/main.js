3db1169076e3c334fe44ff338921a8a5b7631106
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-05-01 12:45:50 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -5,54 +5,6 @@ Parse.Cloud.define('hello', function(req, res) {
 
 
 
-
-Parse.Cloud.define("getUserId", function(request, response) 
-{
-    //Example where an objectId is passed to a cloud function.
-    //var id = request.params.objectId;
-	 	    var id = "Beauty Encounter";
-
-
-    //When getUser(id) is called a promise is returned. Notice the .then this means that once the promise is fulfilled it will continue. See getUser() function below.
-    getUser(id).then
-    (   
-        //When the promise is fulfilled function(user) fires, and now we have our USER!
-        function(user)
-        {
-            response.success(user);
-        }
-        ,
-        function(error)
-        {
-            response.error(error);
-        }
-    );
-
-});
-
-function getUser(userId)
-{
-  //  Parse.Cloud.useMasterKey();
-    var userQuery = new Parse.Query("MyCollection");
-  //  userQuery.equalTo("objectId", userId);
-	userQuery.equalTo("PCompanyName",userId);
-
-    //Here you aren't directly returning a user, but you are returning a function that will sometime in the future return a user. This is considered a promise.
-    return userQuery.first
-    ({
-        success: function(userRetrieved)
-        {
-            //When the success method fires and you return userRetrieved you fulfill the above promise, and the userRetrieved continues up the chain.
-            return userRetrieved;
-        },
-        error: function(error)
-        {
-            return error;
-        }
-    });
-};
-
-
 // Parse.Cloud.define("getUserId", function(request, response) 
 // {
 //     //Example where an objectId is passed to a cloud function.
@@ -183,7 +135,7 @@ query.find({
 
 Parse.Cloud.define("All", function(request, response) {
   var query = new Parse.Query("MyCollection");
-	query.equalTo("PCompanyName","Beauty Encounter")
+	query.equalTo("PCompanyName","Beauty Encounter");
  
  query.find().then(function (res) 
      {
