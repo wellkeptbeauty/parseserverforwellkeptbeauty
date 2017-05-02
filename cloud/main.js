bfa209fa5dfac03163d73c8e700eba3f22780474
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-05-02 12:13:07 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -231,40 +231,40 @@ Parse.Cloud.define("All", function(request, response) {
 //     });
 //  });
 
-// Parse.Cloud.define('All', function(request, status)  
-// {
-//    // res.success('ALL');
+Parse.Cloud.define('All', function(request, status)  
+{
+   // res.success('ALL');
 
-//   var query=new Parse.Query("MyCollection");
-//    query.find().then(function (res) 
-// {
-//  console.log("after query is "+res);
+  var query=new Parse.Query("MyCollection");
+   query.find().then(function (res) 
+{
+ console.log("after query is "+res);
 
-// for (var i=0; i< res.length;i++){
-// var expirydate=res[i].get('PExpirationDate');
+for (var i=0; i< res.length;i++){
+var expirydate=res[i].get('PExpirationDate');
  
-// var inputDate = new Date(expirydate);
-// var todaysDate = new Date();
+var inputDate = new Date(expirydate);
+var todaysDate = new Date();
 
-// if((inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)))
-// {
-// console.log("object id is"+res[i].get('_p_PurchasedUserID'));
-//  // alert("equal")
+if((inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)))
+{
+console.log("object id is"+res[i].get('_p_PurchasedUserID'));
+ // alert("equal")
 
-// //res.success("object id is"+JSON.stringify(result));
-// }
-// else
-// {
+//res.success("object id is"+JSON.stringify(result));
+}
+else
+{
 
-// //alert(" not equal")
-// }
-// }
-//  status.success("final result " + results);
+//alert(" not equal")
+}
+}
+ status.success("final result " + results);
         
       
    
-//     }, function queryFailed(reason) {
-//       status.error("query unsuccessful, length of result " + result.length + ", error:" + error.code + " " + error.message);
+    }, function queryFailed(reason) {
+      status.error("query unsuccessful, length of result " + result.length + ", error:" + error.code + " " + error.message);
          
-//     });
-// });
+    });
+});
