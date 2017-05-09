c43a859bd92282ec9e5d48ed9e21d06cafae09da
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-05-10 01:47:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-   push: JSON.parse(process.env.PARSE_SERVER_PUSH || "{}"),
+//    push: JSON.parse(process.env.PARSE_SERVER_PUSH || "{}"),
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
