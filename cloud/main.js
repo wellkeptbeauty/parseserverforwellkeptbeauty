fc0772ba06f6d2cabef15b7f2e337af40395a0f6
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-05-10 16:04:14 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -501,11 +501,11 @@ Parse.Cloud.define("iosPushforsingleuser", function(request, response) {
 	 var query = new Parse.Query(Parse.Installation);
   query.exists("deviceToken");
 	//query.equalTo('deviceToken','f4ac9341c3598e4c2e5e41ebe1f9f0d631a4fa387873e2c36449743bd2800d8a');
-      //query.equalTo('userId',email);
-	//console.log("inner query is",email);
-		query.equalTo('userId','karthik@betabulls.com');
-	var title="product";
-var message="Hello Beautiful! your "+title +"expirs today.Make sure to toss it and order a new one!";
+      query.equalTo('userId',email);
+	console.log("inner query is",email);
+		//query.equalTo('userId','karthik@betabulls.com');
+	//var title="product";
+var message="Hello Beautiful! your "+request.params.toproducttitle +"expirs today.Make sure to toss it and order a new one!";
 
   // here you can add other conditions e.g. to send a push to sepcific users or channel etc.
 
