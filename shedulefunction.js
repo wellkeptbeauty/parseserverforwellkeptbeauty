d490189513c524544f1dac76b2991e6eb434b4be
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-05-11 15:59:43 +0530

    Update shedulefunction.js

--- shedulefunction.js
+++ shedulefunction.js
@@ -9,7 +9,27 @@ var Parse = require('parse/node');
 Parse.initialize('8PsWDzjb8gZN8I2ytJCKQiA4wP8hiL5jRt4hecwI', 'XlEdqca7cbDXkjU47QUJsjzFWY2K50jWfpDWTSkh', '41tclTmcekJEnOzBcHTULQptqfndEMiAtN6VeGin');
 Parse.serverURL = 'https://wellkeptbeauty.herokuapp.com/parse/';
 
-// var Parse = require(‘parse/node’);
-// Parse.initialize(process.env.8PsWDzjb8gZN8I2ytJCKQiA4wP8hiL5jRt4hecwI, process.env.XlEdqca7cbDXkjU47QUJsjzFWY2K50jWfpDWTSkh, process.env.41tclTmcekJEnOzBcHTULQptqfndEMiAtN6VeGin);
-// Parse.serverURL = process.env.https://wellkeptbeauty.herokuapp.com/parse/;
-Parse.Cloud.run(“Allobjects”);
+ var query = new Parse.Query("MyCollection");
+        query.include('PurchasedUserID');
+        query.find().then(function(res) {
+            console.log("after query is :" + JSON.stringify(res));
+            for (var i = 0; i < res.length; i++) {
+                var expirydate = res[i].get('PExpirationDate');
+                var inputDate = new Date(expirydate);
+                var todaysDate = new Date();
+                if ((inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0))) {
+                    console.log("object id is" + res[i].get('PurchasedUserID').get('username'));
+
+                       console.log("equal");
+                    //res.success("object id is"+JSON.stringify(result));
+                } else {
+                        console.log("iosPushforsingleuser not equal :")
+                }
+            }
+                        console.log("final result :",results)
+            status.success("final result " + results);
+            
+        }, function queryFailed(reason) {
+                        console.log("queryFailed  :",reason)
+            status.error("query unsuccessful, length of result " + result.length + ", error:" + error.code + " " + error.message);
+        });
