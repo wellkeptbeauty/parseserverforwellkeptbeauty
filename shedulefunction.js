253753bbd689433576c72ed1043e528a6f98f165
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-05-11 15:15:15 +0530

    Update shedulefunction.js

--- shedulefunction.js
+++ shedulefunction.js
@@ -5,7 +5,6 @@ function mycoolection() {
     console.log("hai from scheduler");
 }
 mycoolection();
-
 var Parse = require('parse/node');
 Parse.initialize('8PsWDzjb8gZN8I2ytJCKQiA4wP8hiL5jRt4hecwI', 'XlEdqca7cbDXkjU47QUJsjzFWY2K50jWfpDWTSkh', '41tclTmcekJEnOzBcHTULQptqfndEMiAtN6VeGin');
 Parse.serverURL = 'https://wellkeptbeauty.herokuapp.com/parse/';
@@ -16,43 +15,37 @@ Parse.serverURL = 'https://wellkeptbeauty.herokuapp.com/parse/';
         var query = new Parse.Query("MyCollection");
         query.include('PurchasedUserID');
         query.find().then(function(res) {
-            // console.log("after query is :" + JSON.stringify(res));
-
+            console.log("after query is :" + JSON.stringify(res));
             for (var i = 0; i < res.length; i++) {
                 var expirydate = res[i].get('PExpirationDate');
-
                 var inputDate = new Date(expirydate);
                 var todaysDate = new Date();
-
                 if ((inputDate.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0))) {
                     console.log("object id is" + res[i].get('PurchasedUserID').get('username'));
-                    //  console.log("object id is"+res[i].get('PurchasedUserID').get('username'));
-
                     Parse.Cloud.run("iosPushforsingleuser", {
-                        toEmail: res[i].get('PurchasedUserID').get('username'),
+                      console.log("iosPushforsingleuser is :");
+                       toEmail: res[i].get('PurchasedUserID').get('username'),
                         toProductTitle: res[i].get('PProductName')
                     }).then(function(result) {
-                        // make sure the set the enail sent flag on the object
-                        console.log("result :" + JSON.stringify(result))
+                        console.log("iosPushforsingleuser result :" + JSON.stringify(result))
                     }, function(error) {
-
+                        console.log("iosPushforsingleuser error :" + JSON.stringify(error))
                     });
                     //res[i].get('userName').get('userObjectId')
                     // alert("equal")
 
                     //res.success("object id is"+JSON.stringify(result));
                 } else {
-
+                        console.log("iosPushforsingleuser not equal :")
                     //alert(" not equal")
                 }
             }
+                        console.log("final result :",results)
             status.success("final result " + results);
-
-
-
+            
         }, function queryFailed(reason) {
+                        console.log("queryFailed  :",reason)
             status.error("query unsuccessful, length of result " + result.length + ", error:" + error.code + " " + error.message);
-
         });
     
     //fetchmycollection();
