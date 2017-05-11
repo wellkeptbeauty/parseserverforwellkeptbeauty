8c3bcca917d05c72cc3ffc4e1aa4b69d07bf86f5
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-05-11 12:38:02 +0530

    Update shedulefunction.js

--- shedulefunction.js
+++ shedulefunction.js
@@ -1,3 +1,24 @@
 
     var time = new Date();
     console.log('Top folder time shedule ',time);
+var http = require('http');
+
+// console.log('Creating a simple HTTP request');
+
+// http.get("http://api.ipify.org?format=json", function(res) {
+//   var body = ''; // Will contain the final response
+//   // Received data is a buffer.
+//   // Adding it to our body
+//   res.on('data', function(data){
+//     body += data;
+//   });
+//   // After the response is completed, parse it and log it to the console
+//   res.on('end', function() {
+//     var parsed = JSON.parse(body);
+//     console.log(parsed);
+//   });
+// })
+// // If any error has occured, log error to console
+// .on('error', function(e) {
+//   console.log("Got error: " + e.message);
+// });
