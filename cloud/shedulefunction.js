78ca6c9a491c1c9896aa8f5d5ee7e4ffa2a62c9f
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-05-11 11:26:35 +0530

    updated new js file

--- cloud/shedulefunction.js
+++ cloud/shedulefunction.js
@@ -1,8 +1,6 @@
 var objTest= require("./main.js");
-
     var time = new Date();
     var jobid=10;
-    objTest.jobScedule(time,jobid)
-
+//    objTest.jobScedule(time,jobid)
     console.log(time);
     console.log(jobid);
