009f6d826086fb57e04290ec9f7d8454e5067770
wellkeptbeauty <wellkeptbeauty222@gmail.com>
2017-11-21 19:37:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var api = new ParseServer({
   push: {
   ios: [
     {
-      pfx:__dirname + '/certificates/development.p12',
+      pfx:__dirname + '/certificates/pushfordevelopmentwell.p12',
       bundleId: 'com.wellkeptbeauty',
       production: false
     }
